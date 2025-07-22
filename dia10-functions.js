52ee9b0b06afb2a138610e4af6e0484451d635b8
César Felipe Forero <cesar23forero@gmail.com>
2025-07-21 19:45:26 -0500

    ractice JavaScript functions and callbacks with interactive button demo

--- dia10-functions.js
+++ dia10-functions.js
@@ -33,4 +33,55 @@ var pickRandomWords = function (words){
     return words[Math.floor(Math.random()*words.length)];
 }
 
-console.log(pickRandomWords(randomWords))
+//console.log(pickRandomWords(randomWords))
+
+function sumar (a,b){
+    return a + b;
+};
+
+//console.log(sumar(5,4))
+
+var multiplicar = function(a,b){
+    return a * b;
+}
+
+// function saludarUsuario (nombre){
+//     return console.log("Hola " + nombre + " bienvenido!")
+// }
+
+// saludarUsuario("Felipe")
+
+// var cuadradoDeUnNumero = function(numero){
+//     return (numero ** 2);
+// }
+//console.log(cuadradoDeUnNumero(2))
+
+function dobleDelNumero (numero){
+    return( numero * 2);
+}
+
+var funcionGuardada = function(numero2){
+    return(numero2 * 2);
+}
+
+//console.log(dobleDelNumero(2), funcionGuardada(3))
+
+const triple = function (x){
+    return x *3; 
+};
+
+function aplicarOperacion (numero, operacion){
+    return operacion(numero);
+}
+
+// console.log(aplicarOperacion(5,triple))
+
+function decirHola (){
+    return "Hola";
+};
+
+function ejecutarFuncion(h){
+    return h()
+};
+
+console.log(ejecutarFuncion(decirHola))
